e0cebc36afbf72ea8b3cdb4c3692e0b42ae00b4e
Callum Murphy-Hale <19550999+ofasgard@users.noreply.github.com>
2026-04-23 22:56:46 +0100

    feat: Implement button handlers
    Divergence from the previous implementation:
    
    Instead of putting all of the progress clock initialisation logic inside the handler, the handlers just create an empty div. It will be the responsibility of the main loop to pick up on that and create a corresponding `ProgressClock` object to track that progress clock, then add the necessary HTML elements inside of the div.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 pub mod clock;
 
+use crate::clock::ProgressClock;
+
 use wasm_bindgen::prelude::*;
 use web_sys::{ HtmlInputElement, HtmlDivElement, PointerEvent };
 use console_error_panic_hook;
+
+use uuid::Uuid;
 
 #[wasm_bindgen]
 pub fn init_panic_hook() {
@@ -11,6 +15,8 @@ pub fn init_panic_hook() {
 
 #[wasm_bindgen(start)]
 fn run() -> Result<(), JsValue> {
+	let mut clocks: Vec<ProgressClock> = Vec::new();
+
 	// Get a handle to the document.
 	let window = web_sys::window().expect("no window function");
 	let document = window.document().expect("should have a document on window");
@@ -26,12 +32,24 @@ fn run() -> Result<(), JsValue> {
 	let clock_area : HtmlDivElement = clock_area.dyn_into::<HtmlDivElement>().map_err(|_| ()).unwrap();
 	
 	// Set up event handlers for "add clock" buttons.
+	let doc = document.clone();
 	let positive_button_handler = Closure::<dyn FnMut(_)>::new(move |e: PointerEvent| {
-		todo!("Positive button handler not implemented.");
+		let div = doc.create_element("div").unwrap();
+		div.set_id(Uuid::new_v4().to_string().as_str());
+		div.set_class_name("positive");
+		
+		let body = doc.body().expect("document should have a body");
+		body.append_child(&div).unwrap();
 	});
 	
+	let doc = document.clone();
 	let negative_button_handler = Closure::<dyn FnMut(_)>::new(move |e: PointerEvent| {
-		todo!("Negative button handler not implemented.");
+		let div = doc.create_element("div").unwrap();
+		div.set_id(Uuid::new_v4().to_string().as_str());
+		div.set_class_name("negative");
+		
+		let body = doc.body().expect("document should have a body");
+		body.append_child(&div).unwrap();
 	});	
 	
 	positive_button.add_event_listener_with_callback("click", positive_button_handler.as_ref().unchecked_ref())?;
@@ -39,6 +57,9 @@ fn run() -> Result<(), JsValue> {
 	
 	positive_button_handler.forget();
 	negative_button_handler.forget();
+	
+	// The "add good clock" and "add bad clock" buttons just add an empty div to the page with an id that corresponds to a UUIDv4.
+	// We'll need a loop to monitor for new divs, then create a `ProgressClock` to track it and initialise the canvas etc.
 	
 	Ok(())
 }
