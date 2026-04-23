9e0ab7cebd77fa35b57da46071f8181a23f04e4c
Callum Murphy-Hale <19550999+ofasgard@users.noreply.github.com>
2026-04-23 22:34:42 +0100

    feat: Add dummy button handlers
    The closures currently just throw a panic when invoked. Also added `console_error_panic_hook` to display errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-use wasm_bindgen::prelude::*;
-use web_sys::{ HtmlInputElement, HtmlDivElement };
-
 pub mod clock;
+
+use wasm_bindgen::prelude::*;
+use web_sys::{ HtmlInputElement, HtmlDivElement, PointerEvent };
+use console_error_panic_hook;
+
+#[wasm_bindgen]
+pub fn init_panic_hook() {
+    console_error_panic_hook::set_once();
+}
 
 #[wasm_bindgen(start)]
 fn run() -> Result<(), JsValue> {
@@ -18,6 +24,21 @@ fn run() -> Result<(), JsValue> {
 	
 	let clock_area = document.get_element_by_id("clock-area").unwrap();
 	let clock_area : HtmlDivElement = clock_area.dyn_into::<HtmlDivElement>().map_err(|_| ()).unwrap();
+	
+	// Set up event handlers for "add clock" buttons.
+	let positive_button_handler = Closure::<dyn FnMut(_)>::new(move |e: PointerEvent| {
+		todo!("Positive button handler not implemented.");
+	});
+	
+	let negative_button_handler = Closure::<dyn FnMut(_)>::new(move |e: PointerEvent| {
+		todo!("Negative button handler not implemented.");
+	});	
+	
+	positive_button.add_event_listener_with_callback("click", positive_button_handler.as_ref().unchecked_ref())?;
+	negative_button.add_event_listener_with_callback("click", negative_button_handler.as_ref().unchecked_ref())?;
+	
+	positive_button_handler.forget();
+	negative_button_handler.forget();
 	
 	Ok(())
 }
